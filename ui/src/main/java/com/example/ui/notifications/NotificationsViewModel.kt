5f097adf87b5e7959a174ab0f63afb330a67d10c
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-01-26 16:49:46 +0200

    make notifications ViewModel inherit from base ViewModel

--- ui/src/main/java/com/example/ui/notifications/NotificationsViewModel.kt
+++ ui/src/main/java/com/example/ui/notifications/NotificationsViewModel.kt
@@ -2,28 +2,25 @@ package com.example.ui.notifications
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.GetNotificationsUseCase
 import com.example.domain.model.State
+import com.example.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @RequiresApi(Build.VERSION_CODES.O)
 @HiltViewModel
-class NotificationsViewModel @Inject constructor(private val getNotificationsUseCase: GetNotificationsUseCase) : ViewModel(), INotificationsInteractions {
-    private val _state = MutableStateFlow(NotificationUIState())
-    val state = _state.asStateFlow()
+class NotificationsViewModel @Inject constructor(private val getNotificationsUseCase: GetNotificationsUseCase) :
+    BaseViewModel<NotificationUIState>(NotificationUIState()), INotificationsInteractions {
 
     init {
         getNotifications()
     }
 
-    private fun getNotifications(){
+    private fun getNotifications() {
         viewModelScope.launch {
             getNotificationsUseCase().collect { state ->
                 _state.value = when (state) {
@@ -36,8 +33,8 @@ class NotificationsViewModel @Inject constructor(private val getNotificationsUse
     }
 
 
-    override fun onDismiss(id: String){
-        _state.update{ it.copy(notifications = it.notifications.filterNot { it.id == id })}
+    override fun onDismiss(id: String) {
+        _state.update { it.copy(notifications = it.notifications.filterNot { it.id == id }) }
         //todo: tell api/use-case to delete the notification
     }
 
