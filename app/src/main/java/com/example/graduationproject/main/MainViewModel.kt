5353bbdb9965918958b4b07767212f08e4731168
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-02-27 17:20:12 +0200

    minor bug fix

--- app/src/main/java/com/example/graduationproject/main/MainViewModel.kt
+++ app/src/main/java/com/example/graduationproject/main/MainViewModel.kt
@@ -11,7 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val customizeProfileSettings: CustomizeProfileSettingsUseCase
-) : BaseViewModel<Boolean>(false) {
+) : BaseViewModel<Boolean, Nothing>(false) {
     init { viewModelScope.launch(Dispatchers.IO) { isDarkTheme() } }
 
 
