bfc4f86fc3324d2294656e4bc46d9675d0145d1a
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-03-10 06:41:54 +0200

    clean unneeded code

--- data/src/main/java/com/example/data/source/remote/PostRemoteDataSource.kt
+++ data/src/main/java/com/example/data/source/remote/PostRemoteDataSource.kt
@@ -5,7 +5,6 @@ import com.example.data.model.response.PostItemDto
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Multipart
@@ -14,10 +13,6 @@ import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface PostRemoteDataSource {
-
-
-//    @GET("products")
-//    suspend fun getProducts(@Query("limit") amount: Int? = null): Response<List<ProductResponse>>
 
     @GET("post/{post_id}")
     suspend fun getPost(@Path("post_id") postId: String): Response<ApiResponseDto<PostItemDto>>
@@ -33,13 +28,9 @@ interface PostRemoteDataSource {
         @Part fcategory: List<MultipartBody.Part>?
     ): Response<ApiResponseDto<Any>>
 
-    @POST("post/update")
-    suspend fun updatePost(@Body body: PostItemDto): Response<ApiResponseDto<Any>>
-
     @Multipart
     @POST("post/update")
     suspend fun updatePost(
-        //todo: check api does not accept images here?
         @Part images: List<MultipartBody.Part>?,
         @Part("name") name: RequestBody,
         @Part("place") place: RequestBody,
