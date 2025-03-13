2bb2bcef17eb3ead9997996704f7cf71d81beafe
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-03-13 11:38:11 +0200

    increase timeout Time

--- app/src/main/java/com/example/graduationproject/di/NetworkModule.kt
+++ app/src/main/java/com/example/graduationproject/di/NetworkModule.kt
@@ -5,8 +5,8 @@ import com.example.data.source.remote.HomeRemoteDataSource
 import com.example.data.source.remote.NotificationsRemoteDataSource
 import com.example.data.source.remote.OfferRemoteDataSource
 import com.example.data.source.remote.PostRemoteDataSource
-import com.example.data.source.remote.SearchRemoteDataSource
 import com.example.data.source.remote.ProfileDataSource
+import com.example.data.source.remote.SearchRemoteDataSource
 import com.example.data.util.StatusAwareConverterFactory
 import com.example.data.util.TokenInterceptor
 import com.google.gson.Gson
@@ -17,6 +17,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -25,18 +26,25 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(tokenInterceptor: TokenInterceptor): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         val BASE_URL = "https://swapwise.shop/api/"
         val gson = Gson()
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(StatusAwareConverterFactory(gson))
             .addConverterFactory(GsonConverterFactory.create(gson))
-            .client(
-                OkHttpClient.Builder()
-                    .addInterceptor(tokenInterceptor)
-                    .build()
-            )
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(tokenInterceptor: TokenInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(tokenInterceptor)
             .build()
     }
 
