1fb64bba96bfdc424e70d38d8f4d5634d8f996c2
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-03-16 14:10:31 +0200

    fix navigation bar bugs related to search screen

--- ui/src/main/java/com/example/ui/search/SearchRoute.kt
+++ ui/src/main/java/com/example/ui/search/SearchRoute.kt
@@ -2,25 +2,29 @@ package com.example.ui.search
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.ui.util.Screen
 
-private const val ROUTE = "search"
 
-fun NavController.navigateToSearch(
-    filterCategoryId: String = "",
-    builder: NavOptionsBuilder.() -> Unit = {}
-) {
-    navigate("$ROUTE/$filterCategoryId", builder)
+fun NavController.navigateToSearch(filterCategoryId: String = "") {
+    navigate("${Screen.Search.route}/$filterCategoryId") {
+        popUpTo(graph.findStartDestination().id) {
+            inclusive = true
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 }
 
 fun NavGraphBuilder.searchRoute(navController: NavHostController){
     composable(
-        route = "$ROUTE/{${SearchArgs.FILTER_CATEGORY_ID_ARG}}",
+        route = "${Screen.Search.route}/{${SearchArgs.FILTER_CATEGORY_ID_ARG}}",
         arguments = listOf(
             navArgument(SearchArgs.FILTER_CATEGORY_ID_ARG) { NavType.StringType },
         )
