7a1d3a70aaba57c8c5a99096ba5128568a84d615
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-03-16 14:12:13 +0200

    fix navigation bar bugs related to notifications screen

--- ui/src/main/java/com/example/ui/notifications/NotificationsRoute.kt
+++ ui/src/main/java/com/example/ui/notifications/NotificationsRoute.kt
@@ -1,17 +1,24 @@
 package com.example.ui.notifications
 
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.composable
+import com.example.ui.util.Screen
 
-private const val ROUTE = "notifications"
 
-fun NavController.navigateToNotifications(builder: NavOptionsBuilder.() -> Unit = {}) {
-    navigate(ROUTE, builder)
+fun NavController.navigateToNotifications() {
+    navigate(Screen.Notifications.route) {
+        popUpTo(graph.findStartDestination().id) {
+            inclusive = true
+            saveState = true
+        }
+        launchSingleTop = true
+        restoreState = true
+    }
 }
 
 fun NavGraphBuilder.notificationsRoute(navController: NavHostController){
-    composable(ROUTE) { NotificationsScreen(navController) }
+    composable(Screen.Notifications.route) { NotificationsScreen(navController) }
 }
--- ui/src/main/java/com/example/ui/util/Screen.kt
+++ ui/src/main/java/com/example/ui/util/Screen.kt
@@ -5,6 +5,7 @@ sealed class Screen(val route: String) {
     data object Login : Screen("login_screen")
     data object Home : Screen("home_screen")
     data object Search : Screen("search_screen")
+    data object Notifications : Screen("notifications_screen")
     data object Profile : Screen("profile_screen")
     data object ResetPassword : Screen("reset_password")
 
