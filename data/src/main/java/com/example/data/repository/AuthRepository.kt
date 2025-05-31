276e4c39915c38c76ef0073211543451f8d6c978
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-05-31 18:25:25 +0300

    refactor AuthRepository to handle AuthRemoteDataSource

--- data/src/main/java/com/example/data/repository/AuthRepository.kt
+++ data/src/main/java/com/example/data/repository/AuthRepository.kt
@@ -6,25 +6,28 @@ import com.example.data.model.request.VerifyCodeRequest
 import com.example.data.model.response.AuthDto
 import com.example.data.source.local.AuthLocalDataSource
 import com.example.data.source.remote.AuthRemoteDataSource
-import com.example.data.util.checkResponse
 
 class AuthRepository(
     private val authRemoteDataSource: AuthRemoteDataSource,
     private val authLocalDataSource: AuthLocalDataSource
 ) {
     suspend fun signup(body: SignupRequest) {
-        checkResponse { authRemoteDataSource.signup(body) }
-        authLocalDataSource.saveAccountState(false)
-        authLocalDataSource.saveMobile(body.mobile)
+        val authDto = authRemoteDataSource.signup(body)
+        //todo: enable verify code, then use these 2 lines and delete last 2 lines in this functions
+//        authLocalDataSource.saveAccountState(false)
+//        authLocalDataSource.saveMobile(body.mobile)
+
+        authLocalDataSource.saveUserData(authDto)
+        authLocalDataSource.saveAccountState(true)
     }
 
     suspend fun login(body: LoginRequest) {
-        val authDto = checkResponse { authRemoteDataSource.login(body) }
+        val authDto = authRemoteDataSource.login(body)
         authLocalDataSource.saveUserData(authDto)
     }
 
     suspend fun verifyCode(body: VerifyCodeRequest) {
-        val authDto = checkResponse { authRemoteDataSource.verifyCode(body) }
+        val authDto = authRemoteDataSource.verifyCode(body)
         authLocalDataSource.saveUserData(authDto)
         authLocalDataSource.saveAccountState(true)
     }
