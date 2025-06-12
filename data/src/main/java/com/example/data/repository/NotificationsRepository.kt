92d1bb596029f8afa5d9b3b5ec92ae322dd427b4
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-06-12 18:34:18 +0300

    remove Retrofit dependency from notifications UseCase and Repo

--- data/src/main/java/com/example/data/repository/NotificationsRepository.kt
+++ data/src/main/java/com/example/data/repository/NotificationsRepository.kt
@@ -1,10 +1,10 @@
 package com.example.data.repository
 
 import com.example.data.source.remote.NotificationsRemoteDataSource
-import com.example.data.util.checkResponse
 import javax.inject.Inject
 
-class NotificationsRepository @Inject constructor(private val notificationsRemoteDataSource: NotificationsRemoteDataSource) {
-    suspend fun getNotifications() =
-        checkResponse { notificationsRemoteDataSource.getNotifications() }
+class NotificationsRepository @Inject constructor(
+    private val notificationsRemoteDataSource: NotificationsRemoteDataSource
+) {
+    suspend fun getNotifications() = notificationsRemoteDataSource.getNotifications()
 }
--- data/src/main/java/com/example/data/source/remote/NotificationsRemoteDataSource.kt
+++ data/src/main/java/com/example/data/source/remote/NotificationsRemoteDataSource.kt
@@ -1,14 +1,9 @@
 package com.example.data.source.remote
 
-import com.example.data.model.response.ApiResponseDto
 import com.example.data.model.response.NotificationDto
-import retrofit2.Response
-import retrofit2.http.GET
 
 interface NotificationsRemoteDataSource {
 
-    @GET("notifications")
-    suspend fun getNotifications(
-    ): Response<ApiResponseDto<List<NotificationDto>>>
+    suspend fun getNotifications(): List<NotificationDto>
 
 }
