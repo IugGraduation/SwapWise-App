71a655c3bf8990ce472fab08477d9eb832d755c8
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-06-12 18:28:26 +0300

    delete some unneeded retrofit classes

--- app/src/main/java/com/example/graduationproject/di/ResourcesModule.kt
+++ app/src/main/java/com/example/graduationproject/di/ResourcesModule.kt
@@ -1,6 +1,5 @@
 package com.example.graduationproject.di
 
-import com.example.data.source.remote.ProfileRetrofitDataSource
 import com.example.ui.base.StringsResource
 import com.example.ui.util.StringsResourceImpl
 import dagger.Binds
@@ -8,7 +7,6 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
@@ -17,7 +15,4 @@ abstract class ResourcesModule {
     @ViewModelScoped
     abstract fun bindStringsResource(stringsResource: StringsResourceImpl): StringsResource
 
-    @Singleton
-    @Binds
-    abstract fun bindBindProfileDataSource(profileRetrofitDataSource: ProfileRetrofitDataSource): ProfileRetrofitDataSource
 }
--- app/src/main/java/com/example/graduationproject/di/RetrofitModule.kt
+++ app/src/main/java/com/example/graduationproject/di/RetrofitModule.kt
@@ -1,12 +1,5 @@
 package com.example.graduationproject.di
 
-import com.example.data.source.remote.AuthRetrofitDataSource
-import com.example.data.source.remote.HomeRemoteDataSource
-import com.example.data.source.remote.NotificationsRemoteDataSource
-import com.example.data.source.remote.OfferRemoteDataSource
-import com.example.data.source.remote.PostRetrofitDataSource
-import com.example.data.source.remote.ProfileRetrofitDataSource
-import com.example.data.source.remote.SearchRemoteDataSource
 import com.example.data.util.StatusAwareConverterFactory
 import com.example.data.util.TokenInterceptor
 import com.google.gson.Gson
@@ -47,48 +40,5 @@ object RetrofitModule {
             .addInterceptor(tokenInterceptor)
             .build()
     }
-
-    @Singleton
-    @Provides
-    fun providePostApiService(retrofit: Retrofit): PostRetrofitDataSource {
-        return retrofit.create(PostRetrofitDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideOfferApiService(retrofit: Retrofit): OfferRemoteDataSource {
-        return retrofit.create(OfferRemoteDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideAuthenticationApiService(retrofit: Retrofit): AuthRetrofitDataSource {
-        return retrofit.create(AuthRetrofitDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideHomeApiService(retrofit: Retrofit): HomeRemoteDataSource {
-        return retrofit.create(HomeRemoteDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideSearchApiService(retrofit: Retrofit): SearchRemoteDataSource {
-        return retrofit.create(SearchRemoteDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideNotificationsApiService(retrofit: Retrofit): NotificationsRemoteDataSource {
-        return retrofit.create(NotificationsRemoteDataSource::class.java)
-    }
-
-    @Singleton
-    @Provides
-    fun provideProfileApiService(retrofit: Retrofit): ProfileRetrofitDataSource {
-        return retrofit.create(ProfileRetrofitDataSource::class.java)
-    }
-
 
 }
