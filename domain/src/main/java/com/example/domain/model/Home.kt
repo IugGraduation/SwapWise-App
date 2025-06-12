61a62f8ea1d5b815c1b06ef801709201fb6b1cfd
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-06-12 19:44:43 +0300

    remove unneeded code

--- domain/src/main/java/com/example/domain/model/Home.kt
+++ domain/src/main/java/com/example/domain/model/Home.kt
@@ -5,17 +5,7 @@ import com.example.data.model.response.HomeDto
 data class Home(
     val topicsList: List<TopicsHolder> = listOf(),
     val user: User = User()
-) {
-    companion object {
-        fun fromHomeDto(homeDto: HomeDto): Home {
-            val topicsList = homeDto.topicsData?.map { topicDto ->
-                topicDto.toTopicsHolder()
-            }
-            val user = User.fromUserDto(homeDto.user)
-            return Home(topicsList ?: listOf(), user)
-        }
-    }
-}
+)
 
 fun HomeDto.toHome(): Home {
     val topicsList = topicsData?.map { topicDto ->
