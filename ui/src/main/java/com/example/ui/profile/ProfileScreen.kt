eeb4165b7c0b3855d52fec4961813f6dc2ef3634
Sameer-Murtaja <SameerMMurtaja@gmail.com>
2025-12-04 08:08:10 +0200

    use LocalWindowInfo to determine screen height

--- ui/src/main/java/com/example/ui/profile/ProfileScreen.kt
+++ ui/src/main/java/com/example/ui/profile/ProfileScreen.kt
@@ -27,8 +27,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -357,7 +357,7 @@ private fun UserPostsSection(
     modifier: Modifier = Modifier,
 ) {
     LazyColumn(
-        modifier = modifier.heightIn(max = LocalConfiguration.current.screenHeightDp.dp),
+        modifier = modifier.heightIn(max = LocalWindowInfo.current.containerSize.height.dp),
         verticalArrangement = Arrangement.spacedBy(Spacing8)
     ) {
         items(items = state.userPosts) { postItem ->
